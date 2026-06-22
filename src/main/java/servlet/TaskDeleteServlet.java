9e374650da4846cb632904cb3bdc25d0ae3e30db
hideyukifukushima-wq <hideyuki.fukushima@comsize.com>
2026-06-22 11:07:44 +0900

    タスク削除実装途中

--- src/main/java/servlet/TaskDeleteServlet.java
+++ src/main/java/servlet/TaskDeleteServlet.java
@@ -33,15 +33,6 @@ public class TaskDeleteServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		String taskIdstr = request.getParameter("taskId");
 
@@ -78,5 +69,15 @@ public class TaskDeleteServlet extends HttpServlet {
 			rd.forward(request, response);
 		}
 	}
+	
+		
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
 
 }
