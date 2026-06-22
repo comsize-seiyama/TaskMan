3dcd41308e080b48fc226793b9db853f98ec412d
hideyukifukushima-wq <hideyuki.fukushima@comsize.com>
2026-06-22 16:47:15 +0900

    タスク削除機能実装

--- src/main/java/model/dao/TaskDAO.java
+++ src/main/java/model/dao/TaskDAO.java
@@ -97,4 +97,22 @@ public class TaskDAO {
 
 		return taskBean;
 	}
-}
+	public int deleteTask(int taskId)
+	        throws ClassNotFoundException, SQLException {
+
+	    String sql =
+	            "DELETE FROM t_task " +
+	            "WHERE task_id = ?";
+
+	    int count = 0;
+
+	    try (Connection con = ConnectionManager.getConnection();
+	         PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+	        pstmt.setInt(1, taskId);
+
+	        count = pstmt.executeUpdate();
+	    }
+
+	    return count;
+}}
